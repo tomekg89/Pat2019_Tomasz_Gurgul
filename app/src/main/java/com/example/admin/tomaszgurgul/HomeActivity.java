2d2b461c39cf0632f5c8721e24c2b425d9561f00
Tomek <tgurgul@gmail.com>
2019-01-13 20:42:09 +0100

    add login

--- app/src/main/java/com/example/admin/tomaszgurgul/HomeActivity.java
+++ app/src/main/java/com/example/admin/tomaszgurgul/HomeActivity.java
@@ -3,13 +3,79 @@ package com.example.admin.tomaszgurgul;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+            "(?=.*[0-9])" +         //at least 1 digit
+            "(?=.*[a-z])" +         //at least 1 lower case letter
+            "(?=.*[A-Z])" +         //at least 1 upper case letter
+            //    "(?=.*[a-zA-Z])" +      //any letter
+            //    "(?=.*[@#$%^&+=])" +    //at least 1 special character
+            "(?=\\S+$)" +           //no white spaces
+            ".{8,}" +               //at least 4 characters
+            "$");
+
+    Button LoginBtn;
+    EditText emailEditText, passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        LoginBtn = (Button) findViewById(R.id.loginButton);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        // login button
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkEmail();
+                checkPassword();
+
+
+            }
+        });
+
+    }
+    // check if email is correct
+    private boolean checkEmail() {
+        String emailInput = emailEditText.getText().toString().trim();
+        if (emailInput.isEmpty()) {
+            emailEditText.setError("Email can't be empty");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            emailEditText.setError("Please check if email adress is correct");
+            return false;
+        } else {
+            emailEditText.setError(null);
+            return true;
+        }
+    }
+    // check if password is correct( strong enought)
+    private boolean checkPassword() {
+        String passwordInput = passwordEditText.getText().toString().trim();
+        if (passwordInput.isEmpty()) {
+            passwordEditText.setError("Password can't be empty");
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            passwordEditText.setError("Password too weak");
+            return false;
+        } else {
+            passwordEditText.setError(null);
+            return true;
+        }
+
     }
 
     @Override
